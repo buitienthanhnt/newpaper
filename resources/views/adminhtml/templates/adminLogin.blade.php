bc12d9c8eda0b837eda736c3bb22dc932f7304de
buitienthanhnt@gmail.com <namtanvietnam>
2023-06-08 18:00:06 +0700

    hotfix/user: L-60 update code.

--- resources/views/adminhtml/templates/adminLogin.blade.php
+++ resources/views/adminhtml/templates/adminLogin.blade.php
@@ -1,1 +1,25 @@
-@extends('adminhtml.layouts.base_admin')
+@extends('adminhtml.layouts.body_main')
+
+{{-- @section('body_top_tab')
+    @include('adminhtml.layouts.body_top_tab')
+@endsection --}}
+
+@section('body_footer')
+    @include('adminhtml.layouts.body_footer')
+@endsection
+
+{{-- @section('body_overview')
+    @include('adminhtml.layouts.body_overview')
+@endsection --}}
+
+@section('body_main_conten')
+   <div class="row">
+    <div class="col-md-8">
+        <span>ppppppppppppp</span>
+    </div>
+   </div>
+@endsection
+
+@section('body_left_colum')
+
+@endsection
