221572334ceaced85e80afb36357c8afcfd0d84f
thanhnt <buitienthanhnt@gmail.com>
2023-08-06 18:06:17 +0700

    hotfix/comment-reply: L-71 update code for comment reply.

---
+++ public/assets/frontend/js/commentReply.js
@@ -0,0 +1,34 @@
+function sendReply(data){
+	var element = $(data);
+	let comment_id = element.attr("data-comment-id");
+	let reply_conten = $("#reply-comment-"+comment_id).val();
+	let reply_name = $("#reply-name-"+comment_id).val();
+	let reply_email = $("#reply-email-"+comment_id).val();
+	let url_reply = reply_comment_url+"/"+comment_id;
+	$.ajax({
+		url: url_reply,
+		type: 'POST',
+		contentType: 'application/json',
+		data: JSON.stringify({
+			_token: token,
+			paper_value: paper_value,
+			conten: reply_conten,
+			name: reply_name,
+			email: reply_email
+		}),
+		success: function(result) {
+			console.log("success");
+			if (result) {
+				var _data = JSON.parse(result);
+				if (_data.code == 200) {
+					element.parent().parent().children().first().addClass("alert-success").text("success!").show();
+				} else {
+					element.parent().parent().children().first().addClass("alert-warning").text("error!").show();
+				}
+			}
+		}.bind(this),
+		error: function(e) {
+			element.parent().parent().children().first().addClass("alert-warning").text("error!").show();
+		}
+	})
+}
