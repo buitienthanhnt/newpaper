5ba70358b0b19aada5a5af812cd87dca8b672718
thanhnt <buitienthanhnt@gmail.com>
2023-08-20 16:31:59 +0700

    hotfix/category-tree: Update code.

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -18,7 +18,8 @@ class ManagerController extends Controller
     protected $paper;
     protected $category;
     protected $pageTag;
-    const URI = "192.168.100.210";                  // jm-destop
+    // const URI = "192.168.100.210";                  // jm-destop
+    const URI = "192.168.1.214";                    // m6800
     const URI2 = "192.168.1.150/laravel1/public";   // mochi-m4700
 
     public function __construct(
