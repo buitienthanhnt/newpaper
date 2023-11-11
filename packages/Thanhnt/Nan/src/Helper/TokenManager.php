d79a339c17c93e5add79a88d95568b07f4b5f5f9
thanhnt <buitienthanhnt@gmail.com>
2023-11-12 01:56:28 +0700

    main-token: Up code for use token.

---
+++ packages/Thanhnt/Nan/src/Helper/TokenManager.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Thanhnt\Nan\Helper;
+
+require_once('vendor/autoload.php');
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Illuminate\Http\Request;
+
+final class TokenManager
+{
+	private $exp_time = 15 * 60;
+	private $serect_key = 'laravel1.com';
+
+	protected $request;
+
+	function __construct(Request $request)
+	{
+		$this->request = $request;
+	}
+
+	/**
+	 * 
+	 */
+	function getToken(array $data = [], int $iat = 0, int $nbf = 0, int $exp = 0, $type = 'HS256'): array
+	{
+		$iat = $iat ?: time();
+		$exp = $exp ?: $iat + $this->exp_time;
+		try {
+			$payload = array(
+				'iss' => $data, // data cần mã hóa lưu trữ.
+				'iat' => $iat,  // thời gian bắt đầu token.
+				'nbf' => $nbf,  // thời gian token bắt đầu có thể decode(có hiệu lực so với iat).
+				'exp' => $exp,  // thời gian token hết hạn.
+				// 'uId' => $UiD
+			);
+			$jwt = JWT::encode($payload, $this->get_serect_key(), $type);
+			return [
+				'status' => true,
+				'type' => 'HS256',
+				'value' => $jwt
+			];
+		} catch (\Throwable $th) {
+			// echo ($th->getMessage());
+		}
+		return [
+			'status' => false,
+			'type' => 'HS256',
+			'value' => null
+		];
+	}
+
+	/**
+	 * 
+	 */
+	function getTokenData(string $token = '', $type = 'HS256') : array
+	{
+		$token = $token ?: $request_token = $this->request->header('token');
+		if ($token) {
+			try {
+				$decode = JWT::decode($token, new Key($this->get_serect_key(), $type));
+				if (is_object($decode) && $data = $decode->iss) {
+					return (array) $data;
+				}
+			} catch (\Throwable $th) {
+				// echo ($th->getMessage());
+			}
+		}
+		return [];
+	}
+
+	/**
+	 * 
+	 */
+	private function get_serect_key(): string
+	{
+		return $this->serect_key;
+	}
+}
--- packages/Thanhnt/Nan/src/routes/web.php
+++ packages/Thanhnt/Nan/src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Thanhnt\Nan\Helper\TokenManager;
 
 Route::get('/test', function () {
     return asset('public/assets/pub_image/defaul.PNG');
@@ -14,4 +16,18 @@ Route::get('testview', function()  {
 
 Route::any('token/{demo}', function ($demo) {
     return view('nan::token.demo', ['html' => $demo]);
+});
+
+Route::get('getToken', function (Request $request) {
+    $token = new TokenManager($request);
+    // $token_value = $token->getToken(['id' => 1, 'name' => 'nan', 'street' => '21b natial']);
+    $token_value = $token->getToken([]);
+    return $token_value;
+});
+
+Route::get('getTokenData', function (Request $request) {
+    $token = new TokenManager($request);
+    $tokenString = '';
+    $token_value = $token->getTokenData($tokenString);
+    return ($token_value);
 });
