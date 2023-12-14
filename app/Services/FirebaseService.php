af7991a32a82b8553a7f05664bc7fcccae04c5a2
buitienthanhnt@gmail.com <namtanvietnam>
2023-12-14 17:13:47 +0700

    main-firebase: Update code for setup firebase laravel

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -9,8 +9,9 @@ use Kreait\Firebase\Factory;
 // https://firebase-php.readthedocs.io/en/stable/
 // https://github.com/kreait/laravel-firebase#usage
 
-// nên chọn: firebase-adminsdk(gốc) trong list service để tạo key json. 
-// https://console.cloud.google.com/iam-admin/serviceaccounts/details/111481399996955463387/keys?project=react-cli4&supportedpurview=project
+// nên chọn: firebase-adminsdk(gốc) trong list service để tạo key json.
+// https://console.cloud.google.com/iam-admin/serviceaccounts/details/111481399996955463387/keys?project=react-cli4&supportedpurview=project :cli4
+// https://console.cloud.google.com/iam-admin/serviceaccounts/details/109858435023888922380/keys?project=cli6-c4945&supportedpurview=project :cli6
 
 class FirebaseService
 {
