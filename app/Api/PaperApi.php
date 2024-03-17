2ade95294fb332792ab9f4b74db77b38294b4dc2
thanhnt <buitienthanhnt@gmail.com>
2024-03-17 23:19:00 +0700

    dev: update

--- app/Api/PaperApi.php
+++ app/Api/PaperApi.php
@@ -607,7 +607,7 @@ class PaperApi extends BaseApi
 
 	function searchAll()
 	{
-		return $this->search($this->request->get('search'));
+		return $this->search($this->request->get('search', $this->request->get('query')));
 	}
 
 	function search(string $query)
