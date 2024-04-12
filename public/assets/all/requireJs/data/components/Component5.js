1fbdf823ffd44e4bd8d24f353506fe5340f5d1cd
buitienthanhnt@gmail.com <namtanvietnam>
2024-04-12 13:40:00 +0700

    dev: update

--- public/assets/all/requireJs/data/components/Component5.js
+++ public/assets/all/requireJs/data/components/Component5.js
@@ -5,13 +5,61 @@ define(['text!templates/demo5.html', 'viewModal/demo5', 'knockout', 'text!templa
         return ko.applyBindings(new demo5(params.initData), params.element);
     }
 
+    /**
+     * form for add new person
+     */
     ko.components.register('adress-form', {
         viewModel: function (params) {
-            return params.model;
+            self = this;
+            /**
+             * attribute of people
+             */
+            self.id = null;
+            self.age = ko.observable(params.age);
+            self.name = ko.observable(params.name);
+            self.fullName = ko.computed(function () { // gán thuộc tính phụ thuộc
+                return self.name() + " " + self.age();
+            });
+
+            /**
+             * observer message.
+             */
+            self.messageType = ko.observable('');
+            self.message = ko.observable();
+            self.message.subscribe(function () {
+                if (self.message()) {
+                    setTimeout(function () {
+                        self.message(null)
+                    }, 2000)
+                }
+
+            });
+
+            self.onPress = function () {
+                if (self.name() && self.age()) {
+                    params.model.adddItem(
+                        {
+                            name: self.name(),
+                            age: self.age(),
+                            id: params.model.people().length + 1
+                        }
+                    );
+                    self.message('added for new people!')
+                    self.messageType('green')
+                    self.name(null);
+                    self.age(null);
+                } else {
+                    self.message('add fail, please update value!')
+                    self.messageType('red')
+                }
+            }
         },
         template: form
     });
 
+    /**
+     * list of all peoples.
+     */
     ko.components.register('message-editor', {
         viewModel: function (params) {
             return params.model;
--- public/assets/all/requireJs/data/viewModal/demo5.js
+++ public/assets/all/requireJs/data/viewModal/demo5.js
@@ -3,48 +3,7 @@ define(['knockout'], function (ko) {
 
     function Viewmodel5(params) {
         var self = this;
-        self.message = ko.observable();
-        self.message.subscribe(function () {
-            if (self.message()) {
-                setTimeout(function () {
-                    self.message(null)
-                }, 2000)
-            }
-
-        })
-        self.messageType = ko.observable('');
-        self.id = null;
-        self.age = ko.observable(params.age);
-        self.name = ko.observable(params.name);
-        self.fullName = ko.computed(function () { // gán thuộc tính phụ thuộc
-            return self.name() + " " + self.age();
-        });
-
         self.people = ko.observableArray(params.people);
-
-        /**
-         * add new people
-         */
-        self.onPress = function () {
-            // self.age(24);
-            // self.name('after onClick');
-            if (self.name() && self.age()) {
-                self.people.push(
-                    {
-                        name: self.name(),
-                        age: self.age(),
-                        id: self.people().length + 1
-                    }
-                )
-                self.message('added for new people!')
-                self.messageType('green')
-                self.name(null);
-                self.age(null);
-            } else {
-                self.message('add fail, please update value!')
-                self.messageType('red')
-            }
-        }
 
         /**
          * remove an people 
@@ -53,7 +12,13 @@ define(['knockout'], function (ko) {
             self.people.remove(item)
         }
 
-
+        /**
+         * 
+         *  add new people
+         */
+        self.adddItem = function (item) {
+            self.people.push(item)
+        }
     }
 
     return Viewmodel5;
