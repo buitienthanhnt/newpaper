024777067aacad22f8744df2e0820ac80547c514
buitienthanhnt@gmail.com <namtanvietnam>
2024-04-12 11:25:59 +0700

    dev-requireJs: update for use knockoutJs and requireJs

--- public/assets/all/requireJs/data/components/Component5.js
+++ public/assets/all/requireJs/data/components/Component5.js
@@ -1,9 +1,16 @@
 // AMD module whose value is a shared component viewmodel instance
-define(['text!templates/demo5.html', 'viewModal/demo5', 'knockout', 'text!templates/item.html'], function (template, demo5, ko, item) {
+define(['text!templates/demo5.html', 'viewModal/demo5', 'knockout', 'text!templates/item.html', 'text!templates/form.html'], function (template, demo5, ko, item, form) {
     function Component(params = {}) {
         $(params.element).html(template);
         return ko.applyBindings(new demo5(params.initData), params.element);
     }
+
+    ko.components.register('adress-form', {
+        viewModel: function (params) {
+            return params.model;
+        },
+        template: form
+    });
 
     ko.components.register('message-editor', {
         viewModel: function (params) {
