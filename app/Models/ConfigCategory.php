2f649292f33a5dc38a025583cedc642199ad48d3
thanhnt <buitienthanhnt@gmail.com>
2024-09-21 22:34:21 +0700

    dev: update for center_category and source

--- app/Models/ConfigCategory.php
+++ app/Models/ConfigCategory.php
@@ -11,5 +11,6 @@ class ConfigCategory extends Model
     use HasFactory;
     protected $guarded = [];
     const TOP_CATEGORY = "top_category";
+    const CENTER_CATEGORY = "center_category";
     use SoftDeletes;
 }
