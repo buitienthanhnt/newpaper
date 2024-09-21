77fdb96866e57cbd9c290bab66c28bccdd5709ef
thanhnt <buitienthanhnt@gmail.com>
2024-09-21 22:59:56 +0700

    dev: update for setup first database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        // run: php artisan db:seed
 
         DB::table('core_config')->insert([
             ['name' => 'domain', 'value' => 'localhost'],
@@ -27,6 +28,24 @@ class DatabaseSeeder extends Seeder
             ['name' => 'head_conten', 'value'  => 'thông tin nhanh và chính xác nhất'],
             ['name' => 'api_key', 'value'  => null],
             ['name' => 'default_image', 'value'  => 'https://www.gstatic.com/mobilesdk/180622_mobilesdk/clouds@2x.png']
+        ]);
+
+        DB::table('config_categories')->insert([
+            ['name' => 'thời sự', 'active' => 1, 'parent_id'=>0, 'url_alias'=> 'thoi-su', 'type' => 'default'],
+            ['name' => 'trong nước', 'active' => 1, 'parent_id'=>0, 'url_alias'=> 'trong-nuoc', 'type' => 'default'],
+            ['name' => 'quốc tế', 'active' => 1, 'parent_id'=>0, 'url_alias'=> 'quoc-te', 'type' => 'default'],
+            ['name' => 'công nghệ', 'active' => 1, 'parent_id'=>0, 'url_alias'=> 'cong-nghe', 'type' => 'default'],
+            ['name' => 'pháp luật', 'active' => 1, 'parent_id'=>2, 'url_alias'=> 'phap-luat', 'type' => 'default'],
+            ['name' => 'giải trí', 'active' => 1, 'parent_id'=>2, 'url_alias'=> 'giai-tri', 'type' => 'default'],
+            ['name' => 'tin mới', 'active' => 1, 'parent_id'=>0, 'url_alias'=> 'tin-moi', 'type' => 'default'],
+            ['name' => 'chiến sự', 'active' => 1, 'parent_id'=>0, 'url_alias'=> 'chien-su', 'type' => 'time_line'],
+            ['name' => 'thời tiết', 'active' => 1, 'parent_id'=>0, 'url_alias'=> 'thoi-tiet', 'type' => 'time_line'],
+            ['name' => 'kinh tế', 'active' => 1, 'parent_id'=>0, 'url_alias'=> 'kinh-te', 'type' => 'default'],
+        ]);
+
+        DB::table('config_categories')->insert([
+            ['path' => 'top_category', 'value' => '7&2&3&4&8'],
+            ['path' => 'center_category', 'value'  => '7&1&6&5'],
         ]);
     }
 }
