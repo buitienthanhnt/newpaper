053a8d84186b8de2ba3a7e901bc02d0c0be89a4f
buitienthanhnt@gmail.com <namtanvietnam>
2024-09-23 16:55:16 +0700

    dev: update for add default database config when init project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,16 +17,18 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         DB::table('core_config')->insert([
-            ['name' => 'domain', 'value' => 'localhost'],
-            ['name' => 'ip', 'value'  => '192.168.100.156'],
-            ['name' => 'is_windown', 'value'  => false],
-            ['name' => 'Authorization', 'value'  => null],
-            ['name' => 'contact_address', 'value'  => 'Nam Tân-Trực Nội-Trực Ninh-Nam Định'],
-            ['name' => 'contact_phone', 'value'  => '+84 702032201'],
-            ['name' => 'head_title', 'value'  => 'Fast News'],
-            ['name' => 'head_conten', 'value'  => 'thông tin nhanh và chính xác nhất'],
-            ['name' => 'api_key', 'value'  => null],
-            ['name' => 'default_image', 'value'  => 'https://www.gstatic.com/mobilesdk/180622_mobilesdk/clouds@2x.png']
+            ['name' => 'ip', 'value'  => '192.168.100.156', 'type' => 'text'],
+            ['name' => 'main', 'value' => 'laravel1', 'type' => 'text'],
+            ['name' => 'domain', 'value' => 'localhost', 'type' => 'text'],
+            ['name' => 'is_windown', 'value'  => false, 'type' => 'text'],
+            ['name' => 'Authorization', 'value'  => null, 'type' => 'text'],
+            ['name' => 'contact_address', 'value'  => 'Nam Tân-Trực Nội-Trực Ninh-Nam Định', 'type' => 'text'],
+            ['name' => 'contact_phone', 'value'  => '+84 702032201', 'type' => 'text'],
+            ['name' => 'head_title', 'value'  => 'Fast News', 'type' => 'text'],
+            ['name' => 'head_conten', 'value'  => 'thông tin nhanh và chính xác nhất', 'type' => 'text'],
+            ['name' => 'api_key', 'value'  => 'laravel1.com', 'type' => 'text'],
+            ['name' => 'default_image', 'value'  => 'https://www.gstatic.com/mobilesdk/180622_mobilesdk/clouds@2x.png', 'type' => 'text'],
+            ['name' => 'custom_css', 'value'  => "\/* body{filter: grayscale(1)} */", 'type' => 'text'],
         ]);
     }
 }
