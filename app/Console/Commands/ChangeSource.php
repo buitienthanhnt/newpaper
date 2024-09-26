f89e45765c1d0ef4cd6abe6f2ee0aac6a82ae9a9
buitienthanhnt@gmail.com <namtanvietnam>
2024-09-26 11:07:28 +0700

    dev: update for support move product type conten of paper from root table to paper_conten table

--- app/Console/Commands/ChangeSource.php
+++ app/Console/Commands/ChangeSource.php
@@ -6,6 +6,7 @@ use App\Models\Paper;
 use App\Models\PaperContent;
 use Exception;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 use Thanhnt\Nan\Helper\LogTha;
 
 class ChangeSource extends Command
@@ -22,7 +23,7 @@ class ChangeSource extends Command
      *
      * @var string
      */
-    protected $description = 'coppy source from root paper to contents paper table.';
+    protected $description = 'coppy source from root paper(param = [int|all])';
 
     /**
      * @var Paper $paper
@@ -55,8 +56,9 @@ class ChangeSource extends Command
         parent::__construct();
     }
 
-    protected function checkExist(int $paper_id) : bool {
-        $paperConten = PaperContent::where('type','=', 'conten')->where('paper_id', '=', $paper_id)->select()->first();
+    protected function checkExist(int $paper_id): bool
+    {
+        $paperConten = PaperContent::where('type', '=', 'conten')->where('paper_id', '=', $paper_id)->select()->first();
         if ($paperConten && $paperConten->id) {
             return true;
         }
@@ -64,14 +66,47 @@ class ChangeSource extends Command
     }
 
     /**
-     * Execute the console command.
-     *
+     * @param Paper $paper
      * @return void
      */
-    public function handle()
+    function moveProduct($paper): void
+    {
+        if ($paper->type === 'product') {
+            $val = DB::table('price')->where('paper_id', $paper->id)->get()->first();
+            $this->paperContent->create([
+                "type" => PaperContent::TYPE_PRICE,
+                "key" => PaperContent::TYPE_PRICE,
+                "value" => $val->value,
+                "paper_id" => $paper->id,
+                "depend_value" => null,
+            ]);
+        }
+    }
+
+    /**
+     * @param Paper $paper
+     * @return void
+     */
+    function moveSlider($paper): void
+    {
+        return;
+        if ($paper->type === 'carousel') {
+            $slider = $paper->sliderImages();
+            if ($slider->first()) {
+                $this->paperContent->create([
+                    "type" => PaperContent::TYPE_SLIDER,
+                    "key" => PaperContent::TYPE_SLIDER,
+                    "value" => $slider->value,
+                    "paper_id" => $paper->id,
+                    "depend_value" => null,
+                ]);
+            }
+        }
+    }
+
+    protected function moveData($paper_id): void
     {
         try {
-            $paper_id = $this->argument('paper_id');
             if ($this->checkExist($paper_id)) {
                 $this->error('conten of the paper_id has exist!');
                 return;
@@ -83,18 +118,50 @@ class ChangeSource extends Command
             if (!$paper) {
                 throw new Exception("source of the paper_id not found!");
             }
-            $this->paperContent->create([
-                "type" => "conten",
-                "key" => "conten",
-                "value" => $paper->conten,
-                "paper_id" => $paper_id,
-                "depend_value" => null,
-            ]);
+            /**
+             * move for content
+             */
+            if ($paper->conten) {
+                $this->paperContent->create([
+                    "type" => "conten",
+                    "key" => "conten",
+                    "value" => $paper->conten,
+                    "paper_id" => $paper_id,
+                    "depend_value" => null,
+                ]);
+            }
+            /**
+             * move product attribute
+             */
+            $this->moveProduct($paper);
+            /**
+             * move slider carousel
+             */
+            $this->moveSlider($paper);
+
             $this->logTha->logEvent('info', "coppied for paper of: $paper_id ($paper->title)");
             $this->info("coppied for paper of: $paper_id \n");
         } catch (\Throwable $th) {
-            $this->error('-----> '.$th->getMessage());
+            $this->error('-----> ' . $th->getMessage());
             //throw $th;
+        }
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $move_paper = $this->argument('paper_id');
+        if ($move_paper === 'all') {
+            $paper_ids = array_column(Paper::all(['id'])->toArray(), 'id');
+            for ($i = 0; $i < count($paper_ids); $i++) {
+                $this->moveData($paper_ids[$i]);
+            }
+        } else {
+            $this->moveData($move_paper);
         }
     }
 }
